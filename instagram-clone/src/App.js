de84fadb497469c359a3e0f1e655ad3497a0335a
c <celer@MacBook-Air-de-Itzamara.local>
2019-06-23 22:41:41 -0700

    Posts

--- instagram-clone/src/App.js
+++ instagram-clone/src/App.js
@@ -11,12 +11,7 @@ import './App.css';
 class App extends Component {
   render() {
     return <div className="App">
-    <Header />
-    <section className="App-main">
-      <Post nickname="Chris" avatar="https://www.laravelnigeria.com/img/chris.jpg" caption="Moving the community!" image="https://pbs.twimg.com/media/DOXI0IEXkAAkokm.jpg" />
-      <Post nickname="OG" avatar="https://www.laravelnigeria.com/img/chris.jpg" caption="Holding a mic" image="https://pbs.twimg.com/media/DOXI0IEXkAAkokm.jpg" />
-      {/* more posts */}
-    </section>
+  
     <ApolloProvider client={client}>
           <div className="App">
             <Header />
